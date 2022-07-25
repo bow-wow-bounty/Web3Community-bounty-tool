e42f621d53959d4762a761e4ed32f1fc72ad71e3
Rishit Sinha <rishitsinha25@gmail.com>
2022-07-25 15:32:31 +0530

    fix: remove unused exports

--- src/config/auth.js
+++ src/config/auth.js
@@ -5,4 +5,3 @@ export const AUTH_AUDIENCE =
   process.env.NEXT_PUBLIC_SITE_URL || process.env.NEXT_PUBLIC_VERCEL_URL;
 export const AUTH_SECRET = process.env.AUTH_JWT_SECRET;
 export const AUTH_REQUEST_TOKEN_SECRET = process.env.AUTH_REQUEST_SECRET;
-export const AUTH_COOKIES_KEYS = JSON.parse(process.env.AUTH_COOKIES_KEYS);
