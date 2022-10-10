bc1189160b87de4241f05ac2eb349d867fbdef09
bow-wow-bounty <115492195+bow-wow-bounty@users.noreply.github.com>
2022-10-10 19:49:28 +0100

    Update index.js
    change network from Devnet to Mainnet

--- src/modules/wallet-provider/index.js
+++ src/modules/wallet-provider/index.js
@@ -19,7 +19,7 @@ import { useMemo } from "react";
 import { ChildrenPropType } from "../../utils/prop-types";
 
 const WalletProvider = ({ children }) => {
-  const network = WalletAdapterNetwork.Devnet;
+  const network = WalletAdapterNetwork.Mainnet;
 
   const endpoint = useMemo(() => clusterApiUrl(network), [network]);
 
